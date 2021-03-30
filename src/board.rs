25f9ae3482a1a89ac9331d4813660215bfa7a2d3
Omar Mohamed Samir <39497615+omarsamir27@users.noreply.github.com>
2021-03-30 18:27:26 +0200

    draws board properly

--- src/board.rs
+++ src/board.rs
@@ -1,69 +1,53 @@
 use super::tawla_logic;
 use bevy::prelude::*;
 use bevy_mod_picking;
-use bevy_mod_picking::PickableMesh;
+use bevy_mod_picking::{PickSource, PickableMesh};
 
 #[derive(Debug)]
 pub struct TawlaAssets {
     checker_mesh: Handle<Mesh>,
     board_paint: Handle<Texture>,
     board_brown: Handle<StandardMaterial>,
-    checker_black : Handle<StandardMaterial>,
-    checker_white : Handle<StandardMaterial>
+    checker_black: Handle<StandardMaterial>,
+    checker_white: Handle<StandardMaterial>,
 }
-impl FromResources for TawlaAssets{
-    fn from_resources(resources: &Resources) -> Self{
+impl FromResources for TawlaAssets {
+    fn from_resources(resources: &Resources) -> Self {
         let asset_loader = resources.get::<AssetServer>().unwrap();
         let checker_mesh: Handle<Mesh> = asset_loader.load("./mesh/checker.stl");
         let board_paint: Handle<Texture> = asset_loader.load("./art/board.png");
         let mut materials = resources.get_mut::<Assets<StandardMaterial>>().unwrap();
         println!("assets loaded",);
-        TawlaAssets{
+        TawlaAssets {
             checker_mesh,
             board_paint,
-            board_brown : materials.add(StandardMaterial::from(Color::rgb_u8(92, 64, 51))),
-            checker_black : materials.add(StandardMaterial::from(Color::rgb_u8(2, 2, 2))),
-            checker_white : materials.add(StandardMaterial::from(Color::rgb_u8(253, 253,253 )))
+            board_brown: materials.add(StandardMaterial::from(Color::rgb_u8(92, 64, 51))),
+            checker_black: materials.add(StandardMaterial::from(Color::rgb_u8(2, 2, 2))),
+            checker_white: materials.add(StandardMaterial::from(Color::rgb_u8(253, 253, 253))),
         }
     }
-    }
-
-pub fn load_assets(commands: &mut Commands, asset_server: Res<AssetServer>, mut materials: ResMut<Assets<StandardMaterial>>) {
-    let checker_mesh: Handle<Mesh> = asset_server.load("mesh/checker.stl");
-    let board_paint: Handle<Texture> = asset_server.load("art/board.png");
-    commands.insert_resource(TawlaAssets {
-        checker_mesh,
-        board_paint,
-        board_brown : materials.add(StandardMaterial::from(Color::rgb_u8(92, 64, 51))),
-        checker_black : materials.add(StandardMaterial::from(Color::rgb_u8(2, 2, 2))),
-        checker_white : materials.add(StandardMaterial::from(Color::rgb_u8(253, 253,253 )))
-    });
 }
 
- pub fn build_board(
+pub fn build_board(
     commands: &mut Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    tawla_assets: Res<TawlaAssets>
+    tawla_assets: Res<TawlaAssets>,
 ) {
-     println!("here");
     let board_paint: Handle<StandardMaterial> = materials.add(StandardMaterial {
         albedo_texture: Some(tawla_assets.board_paint.clone()),
         shaded: false,
         ..Default::default()
     });
-     println!("here too");
-    println!("{:?}",tawla_assets);
-     /* Build Container Box*/
+    /* Build Container Box*/
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Box::new(27., 31., 0.4))),
-        //material: tawla_assets.board_brown.clone(),
-        material : materials.add(StandardMaterial::from(Color::rgb_u8(10,10,5))),
+        material: tawla_assets.board_brown.clone(),
+        //material : materials.add(StandardMaterial::from(Color::rgb_u8(10,10,5))),
         ..Default::default()
     });
-     println!("after box");
 
-     for i in (-1..=1).step_by(2) {
+    for i in (-1..=1).step_by(2) {
         commands.spawn(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Box::new(0.5, 31., 0.4))),
             material: tawla_assets.board_brown.clone(),
@@ -80,7 +64,7 @@ pub fn load_assets(commands: &mut Commands, asset_server: Res<AssetServer>, mut 
             ..Default::default()
         });
     }
-    /*End Box*/
+    // /*End Box*/
     /*Paint Board*/
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Quad::new(Vec2::new(26., 30.)))),
@@ -100,7 +84,7 @@ pub fn load_assets(commands: &mut Commands, asset_server: Res<AssetServer>, mut 
                         mesh: tawla_assets.checker_mesh.clone(),
                         visible: Visible {
                             is_transparent: true,
-                            is_visible: true,
+                            is_visible: false,
                         },
                         transform: Transform::from_translation(Vec3::new(
                             (k * (12 - 2 * i)) as f32,
@@ -115,6 +99,20 @@ pub fn load_assets(commands: &mut Commands, asset_server: Res<AssetServer>, mut 
         }
     }
     /* Points Spawned*/
+    /* Camera and Light*/
+    commands
+        .spawn(Camera3dBundle {
+            transform: Transform::from_translation(Vec3::new(00.0, 00.0, 50.0))
+                .looking_at(Vec3::default(), Vec3::unit_y()),
+            ..Default::default()
+        })
+        .with(PickSource::default())
+        .spawn(LightBundle {
+            transform: Transform::from_translation(Vec3::new(00.0, 100.0, 100.0))
+                .looking_at(Vec3::default(), Vec3::unit_y()),
+            ..Default::default()
+        });
+    /*End Camera and Light*/
 }
 
 // fn spawn_checker(
